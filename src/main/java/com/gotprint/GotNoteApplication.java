5f6e527d1fda052130230519dfb4cf25c7fad971
piyushpcegarg <piyushpcegarg@gmail.com>
2017-04-16 11:10:38 +0530

    Created Spring boot project using spring initializr.

---
+++ src/main/java/com/gotprint/GotNoteApplication.java
@@ -0,0 +1,12 @@
+package com.gotprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotNoteApplication.class, args);
+	}
+}
